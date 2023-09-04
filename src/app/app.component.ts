db46f9d76a9a89800c2e0e651031a00d8fce2a33
Andó Attila <andoattila698@gmail.com>
2023-09-04 16:25:06 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title:string = 'Frotendelso';
+  tomb:any =["Attila","Béla","Józsi","Dezső","András"]
+}
